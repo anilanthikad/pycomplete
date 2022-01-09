421472481736879d28e72dbceafad3299cd4653d
anilanthikad <anilanthikad@gmail.com>
2022-01-09 12:39:11 +0530

    added function

---
+++ function.py
@@ -0,0 +1,26 @@
+# def greet():
+#     name = input('What is your name?: ')
+#     print(f'Hello, {name}')
+#
+# greet()
+
+# -----------------------------------------------
+
+# Arguments and Parameters
+
+cars = [
+    {'make': 'Ford', 'model': 'Fiesta', 'mileage': 23000, 'fuel_consumed': 460},
+    {'make': 'Ford', 'model': 'Focus', 'mileage': 17000, 'fuel_consumed': 40},
+    {'make': 'Merc', 'model': 'Mx-Shit', 'mileage': 2300, 'fuel_consumed': 60},
+    {'make': 'Mazda', 'model': 'Super', 'mileage': 2300456, 'fuel_consumed': 40}
+]
+
+def calculate_mpg(car_to_calc):
+    mpg = car_to_calc['mileage'] / car_to_calc['fuel_consumed']
+    name = f"{car_to_calc['make']} {car_to_calc['model']}"
+    print(f'{name} does {mpg} miles per gallon.')
+
+calculate_mpg(cars[3])  # for single car
+# or...
+for car in cars:  # for the entire dictionary
+    calculate_mpg(car)
