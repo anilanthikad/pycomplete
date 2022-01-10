969d332b2c745b65e47c474d7b73f37ba20561b7
anilanthikad <anilanthikad@gmail.com>
2022-01-10 11:59:25 +0530

    milestone1 project complete

--- milestone1.py
+++ milestone1.py
@@ -27,13 +27,17 @@ movie = {
 
 
 def menu():
-    user_input = input("Enter 'a' to add a movie, 'l' to see your movie, 'f' to find a movie and 'q' to quit: ")
+    user_input = input("Enter\n"
+                       "'a' to add a movie,\n"
+                       "'l' to see your movie,\n"
+                       "'f' to find a movie and\n"
+                       "'q' to quit: ")
 
     while user_input != 'q':
         if user_input == 'a':
             add_movie()
         elif user_input == 'l':
-            show_movie()
+            show_movie(movies)
         elif user_input == 'f':
             find_movie()
         else:
@@ -45,7 +49,7 @@ def menu():
 def add_movie():
     movie_name = input('What is the movie name?: ')
     movie_director = input('Who is the movie director?: ')
-    movie_year = int(input('Which year was it released? (YYYY): '))
+    movie_year = input('Which year was it released? (YYYY): ')
 
     movies.append({
         'name': movie_name,
@@ -54,8 +58,8 @@ def add_movie():
     })
 
 
-def show_movie():
-    for movie in movies:
+def show_movie(movies_list):
+    for movie in movies_list:
         show_movie_details(movie)
 
 
@@ -63,6 +67,27 @@ def show_movie_details(movie):
     print(f"Name: {movie['name']}")
     print(f"Director: {movie['director']}")
     print(f"Year: {movie['year']}")
+
+
+def find_movie():
+    find_by = input('What property of the movie are you looking for?'
+                    'Name?\n'
+                    'Director?\n'
+                    'Year?: ')
+    looking_for = input('What are you searching for?: ')
+
+    found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
+
+    show_movie(found_movies)
+
+
+def find_by_attribute(items, expected, finder):
+    found = []
+    for i in movies:
+        if finder(i) == expected:
+            found.append(i)
+
+        return found
 
 
 menu()
