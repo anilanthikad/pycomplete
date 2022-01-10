e91e8b2aa61d4b53902aa0bbd980457177211b8c
anilanthikad <anilanthikad@gmail.com>
2022-01-10 17:11:22 +0530

    added a comprehension in milestone1

--- milestone1.py
+++ milestone1.py
@@ -82,12 +82,13 @@ def find_movie():
 
 
 def find_by_attribute(items, expected, finder):
-    found = []
-    for i in items:
-        if finder(i) == expected:
-            found.append(i)
+    return [i for i in items if finder(i) == expected]  # <-- sourcery
 
-    return found
+    # found = []
+    # for i in items:
+    #     if finder(i) == expected:
+    #         found.append(i)
+    # return found
 
 
 menu()
