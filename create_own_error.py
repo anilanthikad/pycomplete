fb7461fe898c9e53b9d30e6a478bf3307310a9dc
anilanthikad <anilanthikad@gmail.com>
2022-01-12 20:57:05 +0530

    own error creation

---
+++ create_own_error.py
@@ -0,0 +1,11 @@
+class MyCustomError(TypeError):
+    """
+    Exception raised when a specific error code is needed.
+    """
+    def __init__(self, message, code):
+        super().__init__(f'Error code {code}: {message}')
+        self.code = code
+
+
+err = MyCustomError('Custom error happened!', 500)
+# print(err.__doc__)
