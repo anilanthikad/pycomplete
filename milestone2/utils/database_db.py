97864a997c05b35836d9bc3516e2da93b1b3063a
anilanthikad <anilanthikad@gmail.com>
2022-01-18 15:36:07 +0530

    milestone2 complete

--- milestone2/utils/database_db.py
+++ milestone2/utils/database_db.py
@@ -12,6 +12,7 @@ Concerned with storing and retrieving books from a json file.
 ]
 """
 
+
 def create_book_table():
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
@@ -46,23 +47,24 @@ def get_all_books():
     return books
 
 
-def _save_all_books(books):
-    with open(books_file, 'w') as file:
-        json.dump(books, file)
-
-
 def mark_book_as_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-    _save_all_books(books)
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('UPDATE books SET read = 1 WHERE name = ?', (name,))
+
+    connection.commit()
+    connection.close()
 
 
 def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book['name'] != name]
-    _save_all_books(books)
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('DELETE FROM books WHERE name = ?', (name,))
+
+    connection.commit()
+    connection.close()
 
 
 
