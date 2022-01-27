1745097aad792b0cd17ae0e4ce958235a24eecd5
anilanthikad <anilanthikad@gmail.com>
2022-01-27 11:06:04 +0530

    decorator 03

--- decorator_simple.py
+++ decorator_simple.py
@@ -1,3 +1,4 @@
+import functools
 
 user = {'username': 'supandi',
         'access_level': 'admin'
@@ -5,15 +6,24 @@ user = {'username': 'supandi',
 
 
 def user_has_permission(func):
+    @functools.wraps(func)
     def secure_func():
         if user.get('access_level') == 'admin':
-            return func
-    return secure_func()
+            return func()
+    return secure_func
 
 
 @user_has_permission
 def my_function():
+    """
+    Allows us to retrieve the password for the admin panel.
+    """
     return 'Password for admin panel is 1234'
 
 
-print(my_function())
+@user_has_permission
+def another():
+    pass
+
+print(my_function.__name__)
+print(another.__name__)
